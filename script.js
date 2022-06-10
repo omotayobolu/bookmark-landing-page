68c3188c9062b8ff736228105f1c665dc7d6fca9
Boluwatife <omotayodaniel10@gmail.com>
2022-06-10 21:54:12 +0100

    Some changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggle = document.querySelector(".toggle");
 const modal = document.querySelector(".modal");
 const faqs = document.querySelectorAll(".faq");
+const tabHeads = document.querySelector(".tab-head")[0];
+const tabBody = document.querySelector(".tab-body")[0];
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
@@ -22,17 +24,3 @@ faqs.forEach((faq) => {
     faq.classList.toggle("active");
   });
 });
-
-const tabHeader = document.querySelector(".tab-header")[0];
-const tabIndicator = document.querySelector(".tab-indicator")[0];
-const tabBody = document.querySelector(".tab-body")[0];
-const tabPane = tabHeader.getElementsByTagName("div");
-
-for (let i = 0; i < tabPane.length; i++) {
-  tabPane[i].addEventListener("click", () => {
-    tabHeader.getElementsByClassName("active")[0].classList.remove("active");
-    tabPane[i].classList.add("active");
-    tabBody.getElementsByClassName("active")[0].classList.remove("active");
-    tabBody.getElementsByTagName("div")[i].classList.add("active");
-  });
-}
