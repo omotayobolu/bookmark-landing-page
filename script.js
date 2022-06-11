68096974959ef78c6cb23cbafc382566201c16f7
Boluwatife <omotayodaniel10@gmail.com>
2022-06-11 20:46:36 +0100

    Some changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const toggle = document.querySelector(".toggle");
 const modal = document.querySelector(".modal");
 const faqs = document.querySelectorAll(".faq");
-const tabHeads = document.querySelector(".tab-head")[0];
-const tabBody = document.querySelector(".tab-body")[0];
+const tab1 = document.querySelector("#tab1");
+const tab2 = document.querySelector("#tab2");
+const tab3 = document.querySelector("#tab3");
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
